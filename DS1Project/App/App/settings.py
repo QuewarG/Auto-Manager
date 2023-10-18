e9295d622c0b0cdb49f44d60643accccc7bc78af
SebastianOviedo <juan.sebastian.oviedo@correounivalle.edu.co>
2023-10-18 09:43:25 -0500

    Se añade conexion a la BD

--- DS1Project/App/App/settings.py
+++ DS1Project/App/App/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = "App.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": 'localhost',
-        "PORT": 5432,
-        "NAME": 'automanager',
-        "USER": 'postgres',
-        "PASSWORD": '123456',
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
